c050ee6cb1e44910eae18fb3d5ecd537cc7cce36
James Lee <leeja9@oregonstate.edu>
2023-01-30 22:17:48 -0800

    initial model, controller, and text view

---
+++ app.py
@@ -0,0 +1,376 @@
+# Oregon State University
+# Author: James Lee
+# Course: CS 361 Winter 2023
+# Assignment: Course Project
+# Description: controller for database. Executes queries
+
+
+import db_conn as dbc
+import datetime
+from getpass import getpass
+
+# 1. STARTUP
+
+
+def _welcome() -> None:
+    '''
+    print welcome message.
+    '''
+    print("Welcome to the Pilates Scheduler!")
+
+
+def _select_menu(menu: str) -> str:
+    '''
+    run selected menu.
+    '''
+    match menu:
+        # 1. Startup
+        case "start":
+            next_menu = _start_menu()
+
+        # 2. Login
+        case "login":
+            next_menu = _login_menu()
+        case "newusr":
+            next_menu = _create_user_menu()
+
+        # 3. Main Menu
+        case "main":
+            next_menu = _main_menu()
+
+        # 4. Available sessions
+        case "avail":
+            next_menu = _available_sessions()
+        case "search_avail":
+            next_menu = _search_available()
+
+        # exit program
+        case _:
+            next_menu = _exit()
+
+    return next_menu
+
+
+def _start_menu() -> str:
+    '''
+    Run start menu actions.
+    Returns key for next menu
+    '''
+    opt = None
+    while not (opt == 1 or opt == 2):
+        print("\n--Start Menu Options--\n"
+              "1. Login\n"
+              "2. Create New User\n")
+
+        opt = _get_opt()
+
+        # quit for 'q' input
+        if opt.upper() == 'Q':
+            return "Q"
+        try:
+            opt = int(opt)
+            if opt == 1:
+                return "login"
+            elif opt == 2:
+                return "newusr"
+            else:
+                print("***Invalid option***")
+        except Exception as e:
+            if type(e) is ValueError:
+                print("\n***Please enter the integer value"
+                      " of the listed options***")
+            else:
+                print(e)
+
+
+def _get_opt() -> str:
+    opt = input("Please enter the integer of an option above ('q' to quit): ")
+    return opt
+
+# 2. Login
+
+
+def _login_menu() -> str:
+    '''
+    Run login menu.
+    Return key for next menu
+    '''
+    usr = None
+    pw = None
+    while (usr is None or pw is None):
+        print("\n--Login Menu--\n")
+
+        # quit for 'q' input
+        usr = _get_username()
+        if usr.upper() == 'Q':
+            return True
+        pw = _get_pw()
+        if pw.upper() == 'Q':
+            return True
+
+        try:
+            name = dbc.validate_login(usr, pw)
+            if name is not None:
+                print("\nHi %s! How can I help you today?" % name)
+                return "main"
+            else:
+                print("\nSorry, please try again...")
+                usr = None
+        except Exception as e:
+            print(e)
+
+
+def _create_user_menu() -> str:
+    '''
+    Create a new user
+    '''
+    usr = None
+    pw = None
+    while (usr is None or pw is None):
+        print("\n--Create New User Menu--\n")
+
+        # quit for 'q' input
+        usr = _get_username()
+        if usr.upper() == 'Q':
+            return "Q"
+        pw = _get_pw()
+        if pw.upper() == 'Q':
+            return "Q"
+        print('Please confirm password')
+        pw2 = _get_pw()
+        if pw2.upper() == 'Q':
+            return "Q"
+        if pw != pw2:
+            print("Passwords didn't match...")
+            usr = None
+            continue
+
+        try:
+            status = dbc.create_user(usr, pw)
+            if status[0] != "S":
+                print(status)
+                usr = None
+            else:
+                print(status)
+                return "start"
+        except Exception as e:
+            print(e)
+
+
+def _get_username() -> str:
+    usr = input("Please enter your Username ('q' to quit): ")
+    return usr
+
+
+def _get_pw() -> str:
+    pw = getpass("Please enter your Password ('q' to quit): ")
+    return pw
+
+# 3. Main menu
+
+
+def _main_menu() -> str:
+    '''
+    show main menu.
+    returns next menu flag
+    '''
+    opt = None
+    while opt is None:
+        print("\n--Main Menu Options--\n"
+              "1. Available sessions\n"
+              "     * Search for currently available sessions\n"
+              "     * Book sessions if available\n"
+              "2. Upcoming sessions\n"
+              "     * Search for upcoming sessions you've booked\n"
+              "3. Session history\n"
+              "     * Search for previously attended sessions\n"
+              "4. Edit personal information\n"
+              "     * edit username, pw, phone, email, etc.\n"
+              "5. Advanced Options (TRAINER OR ADMIN AUTHORIZATION REQUIRED)\n"
+              "     * edit session lists, user authorization, etc.\n")
+
+        opt = _get_opt_main()
+
+        # quit for 'q' input
+        if opt.upper() == 'Q':
+            return "Q"
+        elif opt.upper() == 'MAIN':
+            opt = None
+            continue
+        elif opt.upper() == 'EXIT':
+            return "start"
+
+        try:
+            opt = int(opt)
+            match opt:
+                case 1:
+                    # available sessions
+                    return "avail"
+                case 2:
+                    # upcoming sessions
+                    print("coming soon!")
+                    opt = None
+                case 3:
+                    # session history
+                    print("coming soon!")
+                    opt = None
+                case 4:
+                    # edit personal info
+                    print("coming soon!")
+                    opt = None
+                case 5:
+                    # advanced options
+                    print("coming soon!")
+                    opt = None
+                case _:
+                    print("***Invalid option***")
+                    opt = None
+        except Exception as e:
+            if type(e) is ValueError:
+                print("\n***Please enter the integer value"
+                      " of the listed options***")
+            else:
+                print(e)
+
+
+def _get_opt_main() -> str:
+    '''
+    Get user input for menus after login.
+    '''
+    opt = input("['main' to return to main menu]\n"
+                "['exit' to logout of current session]\n"
+                "['q' to quit program]\n"
+                "Please enter the integer of an option above: ")
+    return opt
+
+
+# 4. Available Sessions
+
+
+def _available_sessions() -> str:
+    '''
+    Show available sessions menu.
+    Return next menu
+    '''
+    opt = None
+    while opt is None:
+        print("\n--Available Session Options--\n"
+              "1. Search Available sessions\n"
+              "     * Search for sessions between two dates\n"
+              "2. Book Available Session\n"
+              "     * Pick a day and time to book request a session\n")
+
+        opt = _get_opt_main()
+
+        # quit for 'q' input
+        if opt.upper() == 'Q':
+            return "Q"
+        elif opt.upper() == 'MAIN':
+            return "main"
+        elif opt.upper() == 'EXIT':
+            return "start"
+
+        try:
+            opt = int(opt)
+            match opt:
+                case 1:
+                    # available sessions
+                    return "search_avail"
+                case 2:
+                    # upcoming sessions
+                    print("coming soon!")
+                    opt = None
+                case _:
+                    print("***Invalid option***")
+                    opt = None
+        except Exception as e:
+            if type(e) is ValueError:
+                print("\n***Please enter the integer value"
+                      " of the listed options***")
+            else:
+                print(e)
+
+
+def _search_available() -> str:
+    '''
+    Search for available sessions between dates.
+    Show list of sessions.
+    Returns next menu
+    '''
+    opt = None
+    while opt is None:
+        print("\n--Search Available Sessions--\n")
+
+        opt = _get_dates()
+
+        # quit for 'q' input
+        if opt.upper() == 'Q':
+            return "Q"
+        elif opt.upper() == 'MAIN':
+            return "main"
+        elif opt.upper() == 'EXIT':
+            return "start"
+        else:
+            return "avail"
+
+
+def _get_dates() -> str:
+    '''
+    get start and end dates for query
+    returns next menu flag
+    '''
+    start_date, end_date = None, None
+    while start_date is None:
+        start_date = input("Please enter a start date to search [mm/dd/yyyy]: ")
+        end_date = input("Please enter an end date..\n: "
+                         "Enter a date [mm/dd/yyyy] OR number of days from start [dd]:\n")
+        try:
+            m, d, y = start_date.split('/')
+            start_date = datetime.date(int(y), int(m), int(d))
+            m, d, y = end_date.split('/')
+            end_date = datetime.date(int(y), int(m), int(d))
+            sessions = dbc.read_sessions((start_date, end_date))
+            print("Here are the available sessions:\n")
+            for session in sessions:
+                print(session)
+            again = input("Search another range? (y/n): ")
+            if again.upper() == 'Y':
+                start_date = None
+                continue
+            else:
+                return 'avail'
+        except Exception as e:
+            print("Please verify your input\n%s\n" % e)
+            start_date = None
+
+# MAIN LOOP
+
+
+def _main_loop() -> None:
+    '''
+    Loop indefinitely until quit flag received
+    '''
+    menu = "start"
+    while menu != "quit":
+        menu = _select_menu(menu)
+
+
+def _exit() -> str:
+    '''
+    Print exit statement.
+    return exit flag
+    '''
+    print("\nExiting... Thanks for using the Pilates Scheduler!")
+    return "quit"
+
+
+# run the program
+def run() -> None:
+    dbc.init_conn()
+    _welcome()
+    _main_loop()
+    dbc.close_conn()
+
+
+if __name__ == '__main__':
+    run()
---
+++ db_conn.py
@@ -0,0 +1,313 @@
+# Oregon State University
+# Author: James Lee
+# Course: CS 361 Winter 2023
+# Assignment: Course Project
+# Description: controller for database. Executes queries
+
+# Imports
+import datetime
+import os
+from mysql import connector
+from mysql.connector import cursor
+from dotenv import load_dotenv
+load_dotenv('./.env')   # load files from ./.env
+
+# Global variables
+_conn = connector.MySQLConnection()
+_cur = cursor.MySQLCursor()
+
+
+# ################
+# CONNECTION SETUP
+# ################
+
+# initialize connection
+def init_conn(HOST: str = os.getenv('DBHOST'),
+              USER: str = os.getenv('DBUSER'),
+              PW: str = os.getenv('DBPW'),
+              DB: str = os.getenv('DB')) -> bool:
+    '''
+    initialize connection for query execution
+    '''
+    global _conn, _cur
+
+    try:
+        if type(HOST) is not str or\
+                type(USER) is not str or\
+                type(PW) is not str or\
+                type(DB) is not str:
+            print("args must be str type. using .env variables...")
+            HOST = os.getenv('DBHOST')
+            USER = os.getenv('DBUSER')
+            PW = os.getenv('DBPW')
+            DB = os.getenv('DB')
+        if _conn.is_connected():
+            close_conn()
+            print('reconnecting...')
+        _conn = connector.connect(host=HOST,
+                                  user=USER,
+                                  password=PW,
+                                  database=DB,
+                                  autocommit=True)
+        _cur = _conn.cursor()
+
+        print("Successfully connected to mysql server: %s@%s"
+              % (USER, HOST))
+        print("Using database: %s" % DB)
+
+        return True
+    except Exception as err:
+        print(err)
+        return False
+
+
+# close connector and cursor
+def close_conn() -> bool:
+    '''
+    Close db connector and cursor.
+    Return status of closing
+    '''
+    global _conn, _cur
+    try:
+        _cur.close()
+        _conn.close()
+        print("Connections closed succesfully.")
+        return True
+    except Exception as err:
+        print(err)
+        return False
+
+
+def change_db(DB: str) -> bool:
+    '''
+    change database if server connected
+    '''
+    global _conn, _cur
+    if type(DB) is not str:
+        print(str(TypeError("DB arg requires str type")))
+        print("Using database: %s" % _conn.database)
+    elif _conn.is_connected():
+        try:
+            _conn.close()
+            _conn.connect(database=DB)
+            _cur.close()
+            _cur = _conn.cursor()
+            print("Using database: %s" % DB)
+        except Exception as e:
+            print("Failed to connect to database.")
+            print(e)
+            init_conn()
+    else:
+        print("not connected to a server...")
+
+
+# ###############
+# CONTROL QUERIES
+# ###############
+
+# #######################
+# CRUD: Sessions Table
+# #######################
+
+# CREATE
+def create_sessions():
+    # TODO
+    pass
+
+
+# READ
+def read_sessions(filter_dates: tuple[datetime.date, datetime.date] = None,
+                  filter_times: tuple[datetime.time, datetime.time] = None)\
+            -> cursor.Generator:
+    '''
+    get list of sessions
+    '''
+    if filter_dates is None:
+        start = datetime.date(2023, 1, 1).isoformat()
+        end = datetime.date
+        end = end.today().isoformat()
+    else:
+        start = filter_dates[0].isoformat()
+        end = filter_dates[1].isoformat()
+        if filter_dates[0] > filter_dates[1]:
+            tmp = end
+            end = start
+            start = tmp
+
+    if filter_times is None:
+        start_time = datetime.time(0, 0).isoformat()
+        end_time = datetime.time(23, 59).isoformat()
+    else:
+        start_time = filter_times[0].isoformat()
+        end_time = filter_times[1].isoformat()
+        if filter_times[0] > filter_times[1]:
+            tmp = start_time
+            start_time = end_time
+            end_time = start_time
+    qry = ("SELECT * FROM Sessions "
+           "WHERE (sessionDate BETWEEN '%s' AND '%s') "
+           "AND (sessionTime BETWEEN '%s' AND '%s')"
+           % (start, end, start_time, end_time))
+    _cur.execute(qry)
+    return _cur
+
+
+# UPDATE
+def update_session():
+    # TODO
+    pass
+
+
+# DELETE
+def delete_session():
+    # TODO
+    pass
+
+
+# #######################
+# CRUD: Users Table
+# #######################
+
+
+# CREATE
+def create_user(usr: str, pw: str, auth: int = 2) -> str:
+    '''
+    Attempt to create user with given usr/pw.
+    Return None if succesful.
+    Return error associated with unsuccessful execution.
+        Note: auth is the authorization level
+        0=admin
+        1=instructor
+        2=client
+    '''
+    query = ("INSERT INTO Users (userName, password, authLvl)"
+             "VALUES ('%s', '%s', '%s')" % (usr, pw, auth))
+    try:
+        _cur.execute(query)
+        return "Successfully added %s to system" % usr
+    except Exception as e:
+        return ("Failed to add %s to system due to error...\n%s"
+                % (usr, e))
+
+
+# READ
+def validate_login(usr: str, pw: str) -> str:
+    '''
+    Validates a username and password.
+    If there is a match, returns name of person.
+    If no match, returns None.
+    '''
+    # TODO: Implement hashing for passwords
+    query = ("SELECT * FROM Users "
+             "WHERE userName='%s' "
+             "AND password='%s'" % (usr, pw))
+    _cur.execute(query)
+    userlist = _cur.fetchall()
+    if len(userlist) == 1:
+        usr_id, usr_name, usr_pw, auth, salt = userlist[0]
+    else:
+        return None
+
+    match auth:
+        case 0:
+            fname = usr_name
+        case 1:
+            query = ("SELECT firstName FROM Instructors "
+                     "WHERE Instructors.userID='%s'" % usr_id)
+            _cur.execute(query)
+            fname = _cur.fetchall()
+            if len(fname == 1):
+                fname = fname[0][0]
+            else:
+                fname = None
+        case 2:
+            query = ("SELECT firstName FROM Clients "
+                     "WHERE Clients.userID='%s'" % usr_id)
+            _cur.execute(query)
+            fname = _cur.fetchall()
+            if len(fname == 1):
+                fname = fname[0][0]
+            else:
+                fname = None
+        case _:
+            fname = None
+
+    return fname
+
+
+# UPDATE
+def update_user():
+    # TODO
+    pass
+
+
+# DELETE
+def delete_user():
+    # TODO
+    pass
+
+
+# #######################
+# CRUD: Clients Table
+# #######################
+
+
+# CREATE
+def create_client():
+    # TODO
+    pass
+
+
+# READ
+def read_client():
+    # TODO
+    pass
+
+
+# UPDATE
+def update_client():
+    # TODO
+    pass
+
+
+# DELETE
+def delete_client():
+    # TODO
+    pass
+
+
+# #######################
+# CRUD: Instructors Table
+# #######################
+
+
+# CREATE
+def create_instructor():
+    # TODO
+    pass
+
+
+# READ
+def read_instructor():
+    # TODO
+    pass
+
+
+# UPDATE
+def update_instructor():
+    # TODO
+    pass
+
+
+# DELETE
+def delete_instructor():
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    init_conn()
+    status = create_user('admin', 'test123')
+    # i, name, pw, a, s = usr[0]
+    print(status)
+    close_conn()
